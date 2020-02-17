0172c4133c57c6288294c17b281b2ff3ab249522
FatBoxer <a.maidansky@gmail.com>
2020-02-17 15:24:58 +0200

    realiset first point of HW

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
+import "fmt"
+
+/*Написать программу для конвертации рублей в доллары.
+Программа запрашивает сумму в рублях и выводит сумму в долларах.
+Курс доллара задайте константой. */
 
 func main() {
+	var rubls float64
+	var dolrs float64
+	const curs float64 = 62.45
 
+
+	fmt.Println("-------======$$$======-------")
+	fmt.Println("Сегодняшний Курс Рубля к Доллару:", curs)
+	fmt.Println("Введите сумму Рублей для продажи:")
+	fmt.Scanln(&rubls)
+
+	dolrs = rubls / curs
+
+	fmt.Println("========")
+	fmt.Println("Привет, в долларах вы получите:")
+	fmt.Printf("%.1f", dolrs)
 }
