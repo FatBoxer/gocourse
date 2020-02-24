ffa89acd4cda9d0c2fa6651292df608989b26c00
FatBoxer <a.maidansky@gmail.com>
2020-02-24 11:10:22 +0200

    final commit for hw2

--- hw2.4_maasiv100/numbers.go
+++ hw2.4_maasiv100/numbers.go
@@ -14,14 +14,32 @@ func main() {
 	rand.Seed(int64(time.Now().Nanosecond()))
 	for i := 0; i < len(array); i++ {
 		array[i] = rand.Intn(100)
-
 	}
 
 	fmt.Println("Заполненный массив различными числами: \n", array)
 
-	//Сортируем массив по возрастанию
+	//сортируем по возростанию
+	var b int
+	var c bool
+	for !c {
+		c = true
+		for i := 0; i < len(array); i++ {
+			if i < len(array)-1 {
+				if array[i] > array[i+1] {
+					b = array[i]
+					array[i] = array[i+1]
+					array[i+1] = b
+					c = false
+				}
+			}
+		}
+	}
+	fmt.Println("Вывод подряд чисел из массива по возрастанию числа: \n", array)
+
+
+
+	}
+
+
 
 	// Все! дальше мозгов не хватает, перехожу к уроку массивы
-
-
-}
